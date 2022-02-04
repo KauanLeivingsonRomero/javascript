846acf703aa172b5d1a029d4c934b782d18f2e92
KauanLeivingsonRomero <96146200+KauanLeivingsonRomero@users.noreply.github.com>
2022-02-03 21:24:59 -0300

    modified

--- Array/ex001/ex001.js
+++ Array/ex001/ex001.js
@@ -1,9 +1,9 @@
 //Crie um script que pergunte 3 números ao usuário, coloque em um array, depois exiba tal array. Modifique os elementos do array de modo a a sequência de números ficar do contrário. Ou seja, se digitou: 1,2,3. Vai aparecer: 3,2,1
-function list() {
-  let valores = document.queryselector(list)
+function lista() {
+  let valores = document.getElementById(list)
   let numeros = []
   numeros.push(valores)
 }
-console.log(numeros)
+console.log(valores)
 
 
