3a21ee1474a7c1f9c50695138702f2c0d8911818
KauanLeivingsonRomero <96146200+KauanLeivingsonRomero@users.noreply.github.com>
2022-02-12 19:51:31 -0300

    HTML e CSS

---
+++ JS DOM/MasterClass Rocketseat/youtube-masterclass-dom-master/main.js
@@ -0,0 +1,15 @@
+// get all keys
+
+const keys = document.querySelectorAll(".key")
+
+
+// play notes
+// function playNote(event)
+
+
+// click with mouse
+
+// keyboard type
+window.addEventListener("keydown", function(event){
+  console.log(event)
+})
